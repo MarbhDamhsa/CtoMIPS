3fe3ae92cbf94fab3b62d8590c11888c50c8ab2e
Jay Mendelsohn <jmendelsohn11@bristolcc.edu>
2017-02-14 17:01:48 -0500

    Update assignmentA1.asm

--- assignmentA1.asm
+++ assignmentA1.asm
@@ -1,33 +1,4 @@
-#############################
-####
-###
-##    Rewrite C statement
-##    B[8] = A[i - j]
-##    in MIPS assembly
-##
-##    i == 11
-##    j == 10
-###
-####
-############################
-############################  Pseudocode to follow:
-############################
-#############
-########
-### int main()
-### {
-###    const int A_SIZE = 2;
-###    const int B_SIZE = 9;
-###    int i = 11;
-###    int j = 10;
-###    int B[ ]; // this makes B[8] a legal array element
-###    int A[ ] = { 0x11, 0x22 }; // this makes A[i - j] legal
-###    
-###    B[8] = A[i - j];
-### }
-########
-#############
-###########################
+
 
     .data
 i:  .word 11
