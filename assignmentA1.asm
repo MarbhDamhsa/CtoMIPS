c4a11b4eef462cde306f689e972e4c0e4e10a38e
Jay Mendelsohn <jmendelsohn11@bristolcc.edu>
2017-02-14 17:01:15 -0500

    Add files via upload

---
+++ assignmentA1.asm
@@ -0,0 +1,57 @@
+#############################
+####
+###
+##    Rewrite C statement
+##    B[8] = A[i - j]
+##    in MIPS assembly
+##
+##    i == 11
+##    j == 10
+###
+####
+############################
+############################  Pseudocode to follow:
+############################
+#############
+########
+### int main()
+### {
+###    const int A_SIZE = 2;
+###    const int B_SIZE = 9;
+###    int i = 11;
+###    int j = 10;
+###    int B[ ]; // this makes B[8] a legal array element
+###    int A[ ] = { 0x11, 0x22 }; // this makes A[i - j] legal
+###    
+###    B[8] = A[i - j];
+### }
+########
+#############
+###########################
+
+    .data
+i:  .word 11
+j:  .word 10
+A:  .word 0x11, 0x22, 0x33
+B:  .word 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8
+    .text
+    la $s3, i          # load address of i
+    la $s4, j          # load address of j
+    la $s6, A          # load address of A
+    la $s7, B          # load address of B
+
+    lw  $t1, ($s3)              # $t1 = i
+    lw  $t1, i                  # $t1 = i does the same
+    lw  $t2, ($s4)              # $t2 = j
+    lw  $t2, j                  #         does the same
+    sub $t0, $t1, $t2           # $t0 = i - j
+    ##########################################
+    # scale array index by 4 ( size of int ) #
+    ##########################################
+    mul $t0, $t0, 4             # $t0 = $t0 * 4
+    lw  $t1, A( $t0 )           # $t1 = A[i - j]
+    
+    ################################################################
+    #### After execution, the program returns the value of 22   ####
+    ################################################################
+    
